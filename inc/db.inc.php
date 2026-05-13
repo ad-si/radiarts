004114675220c66c2968f744c6d5372b6521a149
Adrian Sieber <mail@adriansieber.com>
2026-05-13 14:34:18 +0000

    Seed example artworks on first database initialization

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -2,6 +2,11 @@
 
 $db = new PDO('sqlite:' . __DIR__ . '/../dbs/index.sqlite');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$table_exists = (bool) $db->query(
+    "SELECT name FROM sqlite_master WHERE type='table' AND name='radiartworks'"
+)->fetchColumn();
+
 $db->exec('CREATE TABLE IF NOT EXISTS radiartworks (
     id integer PRIMARY KEY,
     user text,
@@ -13,6 +18,72 @@ $db->exec('CREATE TABLE IF NOT EXISTS radiartworks (
     up integer,
     down integer
 )');
+
+if (!$table_exists) {
+    seed_example_artworks($db);
+}
+
+function seed_example_artworks($db) {
+    $examples = [
+        [
+            'user'   => 'Adrian',
+            'title'  => 'Plus',
+            'x'      => 3,
+            'y'      => 3,
+            'pixels' => [2, 4, 5, 6, 8],
+        ],
+        [
+            'user'   => 'Adrian',
+            'title'  => 'Heart',
+            'x'      => 7,
+            'y'      => 6,
+            'pixels' => [
+                2, 3, 5, 6,
+                8, 9, 10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19, 20, 21,
+                23, 24, 25, 26, 27,
+                31, 32, 33,
+                39,
+            ],
+        ],
+        [
+            'user'   => 'Adrian',
+            'title'  => 'Smiley',
+            'x'      => 8,
+            'y'      => 8,
+            'pixels' => [
+                3, 4, 5, 6,
+                10, 15,
+                17, 19, 22, 24,
+                25, 32,
+                33, 35, 38, 40,
+                41, 44, 45, 48,
+                50, 55,
+                59, 60, 61, 62,
+            ],
+        ],
+    ];
+
+    $insert = $db->prepare(
+        "INSERT INTO radiartworks " .
+        "(id, user, title, array, x, y, time, up, down) " .
+        "VALUES (NULL, :user, :title, :array, :x, :y, datetime('now'), 0, 0)"
+    );
+
+    foreach ($examples as $artwork) {
+        $img = [];
+        foreach ($artwork['pixels'] as $cell) {
+            $img[$cell] = 1;
+        }
+        $insert->execute([
+            ':user'  => $artwork['user'],
+            ':title' => $artwork['title'],
+            ':array' => serialize($img),
+            ':x'     => $artwork['x'],
+            ':y'     => $artwork['y'],
+        ]);
+    }
+}
 
 $db_last_stmt = null;
 
